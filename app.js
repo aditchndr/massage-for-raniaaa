6b31aaf9bdd631c33b4bfb58188b67edd006aaa8
Aditya Chandra <159442478+aditchndr@users.noreply.github.com>
2024-10-13 00:51:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ document.getElementById("days").textContent = day.toString();
 document.getElementById("months").textContent = month.toString();
 document.getElementById("years").textContent = year.toString();
 
+
 let musicPlayer = document.querySelector(".music-container");
 let togglePlayer = document.querySelector(".toggle-player");
 
@@ -33,50 +34,51 @@ let isHidden = true;
 let currentTrack = document.createElement("audio");
 let soundBars = document.querySelector(".sound-bars");
 
-togglePlayer.addEventListener("click", function () {
-  isHidden = !isHidden;
-  if (isHidden) {
-    musicPlayer.classList.remove("hide");
-    togglePlayer.innerHTML = '<img class="w-[98%]" src="icons/close.svg">';
-    trackInfo.style.transitionDelay = "0.4s";
-    trackNav.style.transitionDelay = "0.4s";
-  } else {
-    musicPlayer.classList.add("hide");
-    togglePlayer.innerHTML = '<img class="w-full" src="icons/plus.svg">';
-    trackInfo.style.transitionDelay = "0s";
-    trackNav.style.transitionDelay = "0s";
-  }
+togglePlayer.addEventListener("click", function() {
+    isHidden = !isHidden;
+    if(isHidden){
+        musicPlayer.classList.remove("hide");
+        togglePlayer.innerHTML = '<img class="w-[98%]" src="icons/close.svg">';
+        trackInfo.style.transitionDelay = "0.4s";
+        trackNav.style.transitionDelay = "0.4s";
+    } else {
+        musicPlayer.classList.add("hide");
+        togglePlayer.innerHTML = '<img class="w-full" src="icons/plus.svg">';
+        trackInfo.style.transitionDelay = "0s";
+        trackNav.style.transitionDelay = "0s";
+    }
 });
 
 let soundBarsLottie = bodymovin.loadAnimation({
-  container: soundBars,
-  renderer: "svg",
-  loop: true,
-  autoPLay: false,
-  path: "https://lottie.host/9ec12a7e-e429-453a-9f22-a2af1dcb4dca/2zeuy4rwtP.json",
+    container: soundBars,
+    renderer: "svg",
+    loop: true,
+    autoPLay: false,
+    path: "https://lottie.host/9ec12a7e-e429-453a-9f22-a2af1dcb4dca/2zeuy4rwtP.json",
 });
 
+
 let trackList = [
-  {
-    name: "Penjaga Hati",
-    artist: "Nadhif Basalamah",
-    path: "./music/Penjaga Hati.mp3",
-  },
-  {
-    name: "Get You",
-    artist: "Daniel Caesar",
-    path: "./music/Get You (feat. Kali Uchis).mp3",
-  },
-  {
-    name: "U(You)",
-    artist: "KIDO3008",
-    path: "./music/U(You).mp3",
-  },
-  {
-    name: "Transform",
-    artist: "Daniel Caesar",
-    path: "./music/Transform.mp3",
-  },
+    {
+        name: "Penjaga Hati",
+        artist: "Nadhif Basalamah",
+        path: "./music/Penjaga Hati.mp3",
+    },
+    {
+        name: "Get You",
+        artist: "Daniel Caesar",
+        path: "./music/Get You (feat. Kali Uchis).mp3",
+    },
+    {
+        name: "U(You)",
+        artist: "KIDO3008",
+        path: "./music/U(You).mp3",
+    },
+    {
+        name: "Transform",
+        artist: "Daniel Caesar",
+        path: "./music/Transform.mp3",
+    },
 ];
 
 // EVENT LISTENERS
@@ -84,58 +86,58 @@ playPauseBtn.addEventListener("click", playPauseTrack);
 nextBtn.addEventListener("click", nextTrack);
 prevBtn.addEventListener("click", prevTrack);
 
-function loadTrack(trackIndex) {
-  currentTrack.src = trackList[trackIndex].path;
-  trackName.textContent = trackList[trackIndex].name;
-  trackArtist.textContent = trackList[trackIndex].artist;
-  currentTrack.addEventListener("ended", nextTrack);
-  currentTrack.load();
+function loadTrack(trackIndex){
+    currentTrack.src = trackList[trackIndex].path;
+    trackName.textContent = trackList[trackIndex].name;
+    trackArtist.textContent = trackList[trackIndex].artist;
+    currentTrack.addEventListener("ended", nextTrack);
+    currentTrack.load();
 }
 
 loadTrack(trackIndex);
 
-function playPauseTrack() {
-  if (isPlaying == false) {
-    playTrack();
-  } else {
-    pauseTrack();
-  }
+function playPauseTrack(){
+    if(isPlaying == false){
+        playTrack();
+    }else{
+        pauseTrack();
+    }
 }
 
-function playTrack() {
-  currentTrack.play();
-  isPlaying = true;
-  playPauseBtn.innerHTML = '<img class="w-8" src="icons/pause.svg">';
-  soundBarsLottie.play();
+function playTrack(){
+    currentTrack.play();
+    isPlaying = true;
+    playPauseBtn.innerHTML = '<img class="w-8" src="icons/pause.svg">';
+    soundBarsLottie.play();
 }
 
-function pauseTrack() {
-  currentTrack.pause();
-  isPlaying = false;
-  playPauseBtn.innerHTML = '<img class="w-8" src="icons/play.svg">';
-  soundBarsLottie.stop();
+function pauseTrack(){
+    currentTrack.pause();
+    isPlaying = false;
+    playPauseBtn.innerHTML = '<img class="w-8" src="icons/play.svg">';
+    soundBarsLottie.stop();
 }
 
-function nextTrack() {
-  if (trackIndex < trackList.length - 1) {
-    trackIndex += 1;
-    loadTrack(trackIndex);
-    playTrack();
-  } else {
-    trackIndex = 0;
-    loadTrack(trackIndex);
-    playTrack();
-  }
+function nextTrack(){
+    if(trackIndex < trackList.length - 1){
+        trackIndex += 1;
+        loadTrack(trackIndex);
+        playTrack();
+    }else{
+        trackIndex = 0;
+        loadTrack(trackIndex);
+        playTrack();
+    } 
 }
 
-function prevTrack() {
-  if (trackIndex > 0) {
-    trackIndex -= 1;
-    loadTrack(trackIndex);
-    playTrack();
-  } else {
-    trackIndex = trackList.length - 1;
-    loadTrack(trackIndex);
-    playTrack();
-  }
+function prevTrack(){
+    if(trackIndex > 0){
+        trackIndex -= 1;
+        loadTrack(trackIndex);
+        playTrack();
+    }else{
+        trackIndex = trackList.length - 1;
+        loadTrack(trackIndex);
+        playTrack();
+    }
 }
